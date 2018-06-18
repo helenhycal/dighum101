62ea9f213994b01b82121dab4fb5e81032077e76
Hongye Jiang <Helen@airbears2-10-142-147-33.airbears2.1918.berkeley.edu>
2018-06-18 16:58:51 -0700

    First commot

---
+++ mrassistant.sh
@@ -0,0 +1,18 @@
+echo "Hi there Helen! I'm Mr.Assistant. Today is another new day!"
+printf "\n"
+date
+printf "\n"
+cal
+printf "\n"
+
+echo "Tip #1. Type jupyter notebook to open a jupyter notebook."
+echo "#2. Type screencapture filename.png to take a screenshot."
+echo "#3. Type open somefile.ext to open it."
+echo "#4. Type delete somefile.ext to delete it."
+echo "#5. Type itunes to open itunes app."
+echo "#6. Type rename oldname newname to rename a file."
+echo "#7. Type find 'word' to search files in desktop."
+echo "#8. Type create somefile.ext to create a new file."
+echo "#9. Type inspire!n to randomly display an inspirational quote."
+echo "#10.Type goodbye to exit the terminal."
+.  
---
+++ personal_assistant.sh
@@ -0,0 +1,9 @@
+echo "Hi there Helen! Today is another new day!"
+
+date
+printf "\n"
+
+cal
+
+echo "Tip 1.Deep breath first!"
+
